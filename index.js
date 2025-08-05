ff05a6733687af0a5d7ea2a642fec94f7dcb73ff
Nagalaxmi <laxminaga248@gmail.com>
2025-08-05 10:35:04 +0530

    updations

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the Home Page, changes made!");
+});
+
+app.get('/about', (req, res) => {
+    res.send("This is the About Page");
+});
+
+app.get('/contact', (req, res) => {
+    res.send("Contact us at contact@gmail.com");
+});
